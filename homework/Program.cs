0b23f0e56d9290f891439a217e8a6ca30d381591
e.polovinkin <polkill@list.ru>
2022-11-03 10:39:57 +0300

    sem1hw

---
+++ homework/Program.cs
@@ -0,0 +1,70 @@
+﻿// Console.WriteLine("Hello, World!");
+
+// 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+// Console.Write("ведите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1 > number2)
+// {
+//     Console.WriteLine("первое число больше второго");
+// }
+// else if(number1 < number2)
+// {
+//     Console.WriteLine("второе число больше первого");
+// }
+// else
+// {Console.WriteLine("первое число равняеться второму");
+// }
+
+
+// 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+// Console.WriteLine("введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите третье число: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = number1;
+
+// if(max < number2)
+//     max = number2;
+// if(max < number3)
+//     max = number3;
+// Console.WriteLine($"максимальное число = {max}");
+
+
+// 3.  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+// Console.Write("введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if(number % 2 == 0)
+// {
+//     Console.WriteLine("четное число");
+// }
+// else
+// {
+//     Console.WriteLine("нечетное число");
+// }
+
+
+// 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+// Console.Write("введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int n = 1;
+
+// while(n <= N)
+// {
+//     if (n % 2 == 0)
+//     {
+//         Console.Write($"{n}");
+//     }
+//     n++;
+// }
+// Console.WriteLine();
